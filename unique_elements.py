8f3b80d41a1c074a581e123de87c5aef2c8996c6
Manasa455 <kavurikrishnamanasa22@ifheindia.org>
2025-09-30 14:31:40 +0530

    Create unique_elements.py

---
+++ unique_elements.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+for i in range(n):
+  count = 0
+  for j in range(n):
+    if arr[i] == arr[j]:
+      count += 1
+  if count == 1:
+    print(arr[i], end = " ")
